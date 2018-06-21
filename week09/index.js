b1b0686f97b4d02fbee79d19d86b5b2ce5b9fd6d
kityprincess <stowekm@byui.edu>
2018-06-20 18:36:44 -0700

    Ponder 09 setup

--- week09/index.js
+++ week09/index.js
@@ -2,28 +2,95 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-function doprocessing(req, res) {
+function determineRate(req, res) {
 	// console.log(req.query)
-	var operand1 = Number(req.query.operand1)
-	var operand2 = Number(req.query.operand2)
-	var result = 0
-	switch (req.query.operation)
+	var weight = Number(req.query.weight)
+	var rate = 0
+	switch (req.query.mailType)
 	{
-		case 'add':
-			result = operand1 + operand2
+		case 'ltrstmp':
+			if (weight > 0 && weight <= 1) {
+				rate = '0.50'
+			} else if (weight > 1 && weight <= 2) {
+				rate = '0.71'
+			} else if (weight > 2 && weight <= 3) {
+				rate = '0.92'
+			} else (weight > 3 && <= 3.5) {
+				rate = '1.13'
+			}
 			break
-		case 'subtract':
-			result = operand1 - operand2
+		case 'ltrmtrd':
+			if (weight > 0 && weight <= 1) {
+				rate = '0.47'
+			} else if (weight > 1 && weight <= 2) {
+				rate = '0.68'
+			} else if (weight > 2 && weight <= 3) {
+				rate = ' 0.89'
+			} else (weight > 3 && <= 3.5) {
+				rate = '1.10'
+			}
 			break
-		case 'multiply':
-			result = operand1 * operand2
+		case 'lrgenv':
+			if (weight > 0 && weight <= 1) {
+				rate = '1.00'
+			} else if (weight > 1 && weight <= 2) {
+				rate = '1.21'
+			} else if (weight > 2 && weight <= 3) {
+				rate = '1.42'
+			} else if (weight > 3 && <= 4) {
+				rate = '1.63'
+			} else if (weight > 4 && <= 5) {
+				rate = '1.84'
+			} else if (weight > 5 && <= 6) {
+				rate = '2.05'
+			} else if (weight > 6 && <= 7) {
+				rate = '2.26'
+			} else if (weight > 7 && <= 8) {
+				rate = '2.47'
+			} else if (weight > 8 && <= 9) {
+				rate = '2.68'
+			} else if (weight > 9 && <= 10) {
+				rate = '2.89'
+			} else if (weight > 10 && <= 11) {
+				rate = '3.10'
+			} else if (weight > 11 && <= 12) {
+				rate = '3.31'
+			} else (weight > 12 && <= 13) {
+				rate = '3.52'
+			} 
 			break
-		case 'divide':
-			result = operand1 / operand2
+		case 'pkg':
+			if (weight > 0 && weight <= 1) {
+				rate = '3.50'
+			} else if (weight > 1 && weight <= 2) {
+				rate = '3.50'
+			} else if (weight > 2 && weight <= 3) {
+				rate = '3.50'
+			} else if (weight > 3 && <= 4) {
+				rate = '3.50'
+			} else if (weight > 4 && <= 5) {
+				rate = '3.75'
+			} else if (weight > 5 && <= 6) {
+				rate = '3.75'
+			} else if (weight > 6 && <= 7) {
+				rate = '3.75'
+			} else if (weight > 7 && <= 8) {
+				rate = '3.75'
+			} else if (weight > 8 && <= 9) {
+				rate = '4.10'
+			} else if (weight > 9 && <= 10) {
+				rate = '4.45'
+			} else if (weight > 10 && <= 11) {
+				rate = '4.80'
+			} else if (weight > 11 && <= 12) {
+				rate = '5.15'
+			} else (weight > 12 && <= 13) {
+				rate = '5.50'
+			} 
 			break
 	}
-	// console.log('result is ', result)
-	return result
+	// console.log('rate is ', rate)
+	return rate
 }
 
 express()
@@ -31,20 +98,11 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/math', function(req, res) {
-  		res.render('pages/math', {
-  			theOperation: req.query.operation,
-  			theOperand1: req.query.operand1,
-  			theOperand2: req.query.operand2,
-  			theResult: doprocessing(req, res)
-  		})
-  	})
-  .get('/math_service', function(req, res) {
-  		res.json({
-  			theOperation: req.query.operation,
-  			theOperand1: req.query.operand1,
-  			theOperand2: req.query.operand2,
-  			theResult: doprocessing(req, res)
+  .get('/rates', function(req, res) {
+  		res.render('pages/rates', {
+  			weight: req.query.weight,
+  			mailType: req.query.mailType,
+  			rate: determineRate(req, res)
   		})
   	})
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
